91d686673ee9d352cbc2ed2457eed21c865f2dee
Taki Yasir <95402895+TakiYasirUIU@users.noreply.github.com>
2022-03-25 23:19:00 +0600

    React extension pack and component with css done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState} from 'react';
+import Countries from './components/Countries/Countries';
+import Person from './components/Person/Person';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
-     
+      <Header></Header>
+      <Countries></Countries>
+      <Person></Person>
+
     </div>
   );
 }
 
-function LoadCountries(){
+
+/* function LoadCountries() {
   const [countries, setCountries] = useState([]);
 
-  useEffect( ()=>{
+  useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
-    .then(res=>res.json())
-    .then(data=>setCountries(data));
-  },[])
-  return(
+      .then(res => res.json())
+      .then(data => setCountries(data));
+  }, [])
+  return (
     <div>
       <h1>Visiting Every country in the world!!!</h1>
       <h3>Available Countries: {countries.length}</h3>
+      {
+        countries.map(country => <p><Country name={country.name.common} population={country.population}></Country></p>)
+      }
     </div>
   )
 }
+
+function Country(props){
+  return(
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h3>Population: {props.population}</h3>
+    </div>
+  )
+} */
 
 export default App;
