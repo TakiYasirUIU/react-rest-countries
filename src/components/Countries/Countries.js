9ddb9cef98b65d7970138f936e0c0f08e5cdf725
Taki Yasir <95402895+TakiYasirUIU@users.noreply.github.com>
2022-03-25 23:36:21 +0600

    component inside the folder structure

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Countries = () => {
+    const [countries, setCountries] = useState([]); // Step 1
+    useEffect( ()=>{   // step 2 
+        fetch('https://restcountries.com/v3.1/all')  // Step 3
+        .then(res => res.json())
+        .then(data => setCountries(data)); // Step 4
+    }, [])
+
     return (
         <div>
-            <h2>Countries in other File.</h2>
+            <h1>Hello from Countries: {countries.length }</h1>
+            {
+                countries.map(country => console.log(country))
+            }
         </div>
     );
 };
