342cb5597ca07352507cc5274cc6e6e675e0fcab
Taki Yasir <95402895+TakiYasirUIU@users.noreply.github.com>
2022-03-26 06:32:20 +0600

    add unique key for mapping component

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]); // Step 1
@@ -10,14 +11,18 @@ const Countries = () => {
     }, [])
 
     return (
-        <div>
+        <div >
             <h1>Hello from Countries: {countries.length }</h1>
             {/* {
                 countries.map(country => console.log(country))
             } */}
-            {
-                countries.map(country => <Country country={country}></Country>)
-            }
+            <div className='countries-container'>
+                {
+                countries.map(country => <Country country={country} key={country.cca3}></Country>)
+                }
+
+            </div>
+            
         </div>
     );
 };
